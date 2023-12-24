3c900910accb57df81beb9ecbec9fac87d7eaded
systemkin <154638491+systemkin@users.noreply.github.com>
2023-12-24 14:41:48 +0300

    Update 66. Plus One (With iterators).cpp

--- 66. Plus One (With iterators).cpp
+++ 66. Plus One (With iterators).cpp
@@ -3,7 +3,6 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         for(vector<int>::iterator i = digits.end()-1; i >= digits.begin(); i--)
         {
-            //digits[0] = *i;
             if (*i <= 8)
             {
                 i = digits.insert(i, *i+1);
