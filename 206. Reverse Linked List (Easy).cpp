e5c5afc6419bc9a4de463fa52a1421f95d7449cc
systemkin <154638491+systemkin@users.noreply.github.com>
2023-12-25 18:19:19 +0300

    Create 206. Reverse Linked List (Easy).cpp

---
+++ 206. Reverse Linked List (Easy).cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr)
+            return nullptr;
+        if (head->next == nullptr)
+            return head;
+        ListNode *prev;
+        ListNode *next = head->next;
+        head->next = nullptr;
+        while(next != nullptr)
+        {
+            prev = head;
+            head = next;
+            next = head->next;
+            head->next = prev;
+        }
+        return head;
+
+    }
+};
